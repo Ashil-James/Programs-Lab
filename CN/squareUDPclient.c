3822fd69a539f3b19847cc75ed3d74f5283f05ca
Ashil-James <ashilgjames@gmail.com>
2026-05-06 11:28:50 -0400

    Square using UDP

---
+++ CN/squareUDPclient.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+
+#define PORT 8080
+
+int main(){
+	struct sockaddr_in server, client;
+	socklen_t clilen = sizeof(client);
+	char buffer[1024];
+	int num;
+	char res[1024];
+	
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	printf("Socket Created Successfuly...\n");
+	
+	server.sin_family = AF_INET;
+	server.sin_port = htons(PORT);
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
+	int bind_stat = bind(sockfd, (struct sockaddr*)&server, sizeof(server));
+	printf("Binded Successfuly...\n");
+	
+	printf("Enter the number to send to server: ");
+	scanf("%d", &num);
+	
+	sprintf(buffer,"%d",num);
+	sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&server, sizeof(server));
+	printf("Number sent to server..\n");
+	
+	int recv_bytes = recvfrom(sockfd, res, sizeof(res), 0, (struct sockaddr*)&client, &clilen);
+	res[recv_bytes] = '\0';
+	
+	int ans = atoi(res);
+	printf("Square: %d", ans);
+}
---
+++ CN/squareUDPserver.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<string.h>
+#include<netdb.h>
+
+#define PORT 8080
+
+int main() {
+	
+	struct sockaddr_in server, client;
+	socklen_t clilen = sizeof(client);
+	char buffer[1024];
+	char result[1024];
+	
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	printf("Socket created successfuly...\n");
+	
+	server.sin_family = AF_INET;
+	server.sin_port = htons(PORT);
+	server.sin_addr.s_addr = INADDR_ANY;
+	
+	int bind_status = bind(sockfd, (struct sockaddr*)&server, sizeof(server));
+	printf("Binded Successfuly...\n");
+	
+	int recv_bytes = recvfrom(sockfd, buffer, sizeof(buffer),0, (struct sockaddr*)&client, &clilen);
+	buffer[recv_bytes] = '\0';
+	printf("Number recieved successfuly..\n");
+	
+	int num = atoi(buffer);
+	int square = num * num;
+	printf("Square Computed Successfuly..\n");
+	printf("Square is: %d\n",square);
+	
+	sprintf(result, "%d", square);
+	sendto(sockfd, result, strlen(result), 0,(struct sockaddr*)&client, sizeof(client));
+	printf("Square Sent...\n");
+}
